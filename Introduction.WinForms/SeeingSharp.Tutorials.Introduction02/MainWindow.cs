181e4b3e6d4b3b5cf7d913ddec38942d23f84c55
Roland König <roland.koenig@rolandk.de>
2016-02-22 22:13:42 +0100

    Changed license for tutorials to MIT

--- Introduction.WinForms/SeeingSharp.Tutorials.Introduction02/MainWindow.cs
+++ Introduction.WinForms/SeeingSharp.Tutorials.Introduction02/MainWindow.cs
@@ -1,23 +1,26 @@
 ﻿#region License information (SeeingSharp and all based games/applications)
 /*
-    Seeing# and all games/applications distributed together with it. 
-    More info at 
-     - https://github.com/RolandKoenig/SeeingSharp (sourcecode)
-     - http://www.rolandk.de/wp (the autors homepage, german)
-    Copyright (C) 2016 Roland König (RolandK)
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+    All tutorials of Seeing# are provided under the following license.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
+	The MIT License (MIT)
+	Copyright (c) 2016 Roland Koenig
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy 
+	of this software and associated documentation files (the "Software"), to deal 
+	in the Software without restriction, including without limitation the rights 
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+	of the Software, and to permit persons to whom the Software is furnished to do 
+	so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all 
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 #endregion
 using SeeingSharp.Multimedia.Drawing3D;
